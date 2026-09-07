b87d475f0d3c5bd3cce8f2302c1e9d7015450d4f
muhammadfaseehfaseeh21 <muhammadfaseehfaseeh21@gmail.com>
2026-09-07 09:54:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ env_api_key = os.getenv("GEMINI_API_KEY", "")
 user_api_key = st.sidebar.text_input("Gemini API Key", value=env_api_key, type="password", help="Enter your Google Gemini API key.")
 
 model_choice = st.sidebar.selectbox(
-    "Select Model",["gemini-1.5-flash"]
+    "Select Model", ["gemini_1.5_flash"],
     index=0
 )
 
